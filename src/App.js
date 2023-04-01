94c6399050aee06cbf5546392e89fec33d0bde3a
Ryan <ryjen.pears@gmail.com>
2023-04-01 16:28:23 +0200

    handling the data in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import getPage from './api/content';
+import Grid from './Components/Grid';
 
-function App() {
+const App = () => {
+	// Handles the fetching of the data.
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState([])
+
+	useEffect(() => {
+		// Loads the data
+		getPage()
+			.then(resp => {
+				setData(resp.blocks[0].products)
+				setIsLoading(false);
+			})
+			.catch(error => {
+				console.log('Something went wrong: ' + error)
+			})
+	}, [])
+
+	if (isLoading) {
+		return (
+			<div>
+				Loading data
+			</div>
+		);
+	}
+
 	return (
-		<div>
-
-		</div>
+		<Grid movieData={data} />
 	);
 }
 
